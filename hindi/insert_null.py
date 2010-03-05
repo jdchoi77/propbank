002445047207bc032a794f6edc02a25b38bbfabc
jdchoi77 <jdchoi77@cb446714-5c79-11de-82f7-67359cf3946f>
2010-03-05 17:37:06 +0000

    bigpro, relpro, pro APIs added

--- hindi/insert_null.py
+++ hindi/insert_null.py
@@ -7,16 +7,23 @@ DREL_K1 = 'k1'
 DREL_K2 = 'k2'
 
 # {'aa':'huaa', 'ii':'huii', 'e':'huey', 'ne':'vaale', 'ne':'vaalaa', 'ne':'vaalii'}
-REL_PRO_VERBS = {'आ':'हुआ', 'ई':'हुई', 'ए':'हुए', 'ने':'वाले', 'ने':'वाला', 'ने':'वाली'}
+#REL_PRO_VERBS = {'आ':'हुआ', 'ई':'हुई', 'ए':'हुए', 'ने':'वाले', 'ने':'वाला', 'ने':'वाली'}
+REL_PRO_VERBS = {'ने':'वाले'}
 REL_PRO_DRELS = {'nmod__k1inv', 'nmod__k2inv'}
 REL_PRO       = 'RELPRO'
 
 BIG_PRO_SUFFIX = ['ना', 'नी', 'ने']		# naa, nil, ne
-BIG_PRO_NOT    = 'वाला'				# vaalaa
+BIG_PRO_NOT    = 'वाला'					# vaalaa
+BIG_PRO_KAR    = 'कर'					# kar
+BIG_PRO_TE     = 'ते'					# te
+BIG_PRO_HUEY   = 'हुए'					# huey
+BIG_PRO        = 'BIGPRO'
 
+PRO_AUR        = 'और'
+PRO            = 'PRO'
 
 class InsertNull:
-	# Inserts rel-pro chunks to 'tree'
+	# Inserts rel-pro chunks to 'tree'.
 	# tree : Tree
 	def insertRelPro(self, tree):
 		size = len(tree)
@@ -26,15 +33,15 @@ class InsertNull:
 			chunk = tree[i]; i += 1
 			if not self._isRelProVerb(chunk): continue
 			headId = chunk.getName()
+			if not headId: continue
 			
-			if not chunk.isChild(DREL_K1, headId):
+			if not tree.existChild(DREL_K1, headId):
 				tree.insertFirstChild(headId, self.getNullChunk(REL_PRO, DREL_K1, headId))
 				i += 1; size += 1
 
-			if not chunk.isChild(DREL_K2, headId):
+			if not tree.existChild(DREL_K2, headId):
 				tree.insertLastChild(headId, self.getNullChunk(REL_PRO, DREL_K2, headId))
 				i += 1; size += 1
-				print(tree)
 
 	# Returns true if 'chunk' is a rel-pro verb.
 	# chunk : Chunk
@@ -49,6 +56,70 @@ class InsertNull:
 				return True
 	
 		return False
+
+	# Inserts big-pro chunks to 'tree'.
+	# tree : Tree
+	def insertBigPro(self, tree):
+		size = len(tree)
+		i    = 0
+		
+		while i < size:
+			chunk = tree[i]; i += 1
+			if not self._isBigProVerb(tree, chunk): continue
+			headId = chunk.getName()
+			if not headId: continue
+			
+			if not tree.existChild(DREL_K1, headId):
+				tree.insertFirstChild(headId, self.getNullChunk(BIG_PRO, DREL_K1, headId))
+				i += 1; size += 1
+
+
+	# Returns true if 'chunk' is a big-pro verb.
+	# chunk : Chunk
+	def _isBigProVerb(self, tree, chunk):
+		if not chunk.isVerb()              : return False
+		if chunk[1].wordEquals(BIG_PRO_KAR): return True
+		if len(chunk) < 3                  : return False
+		if chunk[2].wordEquals(BIG_PRO_KAR): return True
+		
+		for suffix in BIG_PRO_SUFFIX:
+			if chunk[1].wordEnds(suffix) and not chunk[2].wordEquals(BIG_PRO_NOT):
+				return True
+				
+		headId = chunk.getName()
+		if not headId: return False
+		if chunk[1].wordEnds(BIG_PRO_TE) and chunk[2].wordEquals(BIG_PRO_HUEY) and not tree.existChild(REL_PRO, headId):
+			return True
+
+		return False
+
+	# Inserts pro chunks to 'tree'.
+	# tree : Tree
+	def insertPro(self, tree):
+		size = len(tree)
+		i    = 0
+		
+		while i < size:
+			chunk = tree[i]; i += 1
+			if not self._isProConjunct(chunk): continue
+			headId = chunk.getName()
+			if not headId: continue
+			verbLinks = tree.getVerbChildren(headId)
+			if not verbLinks: continue
+			
+			for child in verbLinks:
+				childId = child.getName()
+				if not childId: continue
+				if not tree.existChild(DREL_K1, childId):
+					tree.insertFirstChild(childId, self.getNullChunk(PRO, DREL_K1, childId))
+					size += 1
+			
+			i = tree.index(chunk) + 1
+
+	# Returns true if 'chunk' is a conjunct for pro chunk.
+	# chunk : Chunk
+	def _isProConjunct(self, chunk):
+		return chunk.isConjunct() and chunk[1].wordEquals(PRO_AUR)
 
 	# Returns a null chunk.
 	# label: label of the null chunk (e.g., RELPRO, BIGPRO) : string
@@ -70,9 +141,10 @@ ins = InsertNull()
 
 for tree in ssf.getTrees():
 	ins.insertRelPro(tree)
+	ins.insertBigPro(tree)
+	ins.insertPro(tree)
 
-
-#ssf.print(OUT_FILE)
+ssf.print(OUT_FILE)
 
 
 

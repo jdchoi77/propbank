69780dbf7230fc3cbbe792f8e822f86037c45993
jdchoi77 <jdchoi77@cb446714-5c79-11de-82f7-67359cf3946f>
2010-10-28 04:58:54 +0000

    ssf2jubilee updated

--- trunk/hindi/ssf2jubilee.py
+++ trunk/hindi/ssf2jubilee.py
@@ -7,37 +7,53 @@ import sys
 import glob
 from ssf_api import *
 
-SSF_DIR = sys.argv[1]	# directory path containing SSF files
-SSF_EXT = sys.argv[2]	# SSF file extension (e.g., 'prun', 'dat')
-f_map   = open('frameset_map.txt')
-d_map   = dict()
+SSF_DIR  = sys.argv[1]	# directory path containing SSF files
+SSF_EXT  = sys.argv[2]	# SSF file extension (e.g., 'prun', 'dat')
+TREE_DIR = sys.argv[3]
+TASK_DIR = sys.argv[4]
+f_map    = open('frameset_map.txt')
+d_map    = dict()
+
+def makeThreeDigits(i):
+	if   i < 10 : return '00'+str(i)
+	elif i < 100: return '0' +str(i)
+	else        : return str(i)
+
+def getPof(tree, drel, headId):
+	for chunk in tree:
+		if chunk.isChild(drel, headId):
+			return str(chunk[1].tokenId)+':1-ARGM-PRX'
+
+	return None
 
 # retrieve lemma map
 for line in f_map:
 	ls = line.split()
 	d_map[ls[0]] = ls[1]
 
+ls_prop = list()
+
 for ssfFile in glob.glob(SSF_DIR+'/*.'+SSF_EXT):
-	treeFile = ssfFile + '.tree'
-	taskFile = ssfFile + '.task'
+	prefix   = ssfFile[ssfFile.rfind('/')+1:ssfFile.rfind('.')]
+	treeFile = prefix + '.parse'
 	ssf      = SSF(ssfFile)
-	fTree    = open(treeFile, 'w')
-	fTask    = open(taskFile, 'w')
+	fTree    = open(TREE_DIR+'/'+treeFile, 'w')
 	print(ssfFile)
 	
 	for treeId, tree in enumerate(ssf.getTrees()):
 		lTree = ['((SSF']
-		tokId = 0
+		tree.generateTokenIDs()
 		
 		for chunk in tree:
 			lTree.append('    '+chunk.toJubilee())
 			
 			if chunk.isVerb():
-				lProp = [treeFile, str(treeId)]
+				lProp  = [treeFile, str(treeId)]
+				headId = chunk.getName()
 		
-				for i,node in enumerate(chunk[1:]):
+				for node in chunk[1:]:
 					if node.posEquals('VM'):
-						predId = str(tokId + i)
+						predId = str(node.tokenId)
 						lemma  = node.getLemma()
 						
 						if lemma in d_map:
@@ -52,13 +68,25 @@ for ssfFile in glob.glob(SSF_DIR+'/*.'+SSF_EXT):
 						lProp.append('-----')
 						lProp.append(predId+':0-rel')
 						
-						fTask.write(' '.join(lProp)+'\n')
+						prx = getPof(tree, 'pof', headId)
+						if prx: lProp.append(prx)
+						
+						ls_prop.append((lemma, lProp))
 						break
-			
-			tokId += len(chunk) - 1
 	
 		lTree.append('))\n')
 		fTree.write('\n'.join(lTree))
 
 	fTree.close()
-	fTask.close()
+
+ls_prop.sort()
+index = 1
+for i,(lemma, lProp) in enumerate(ls_prop):
+	if i%100 == 0:
+		fTask = open(TASK_DIR+'/'+makeThreeDigits(index)+'.task', 'w')
+		index += 1
+
+	fTask.write(' '.join(lProp)+'\n')
+
+
+
